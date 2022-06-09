5c7bf39302162cc9b40c4fd4c8da8114d3b52a65
Naufal-Tech <naufal.geo11@gmail.com>
2022-06-09 22:31:05 +0700

    latihan

---
+++ router/routerGamePlay.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router();
+let models = require('../models');
+const verifySession = require('../auth/verifySession');
+const gamePlayController = require('../controller/GameplayController');
+
+
+router.get('/trial-game', verifySession, gamePlayController.trial_game);
+
+router.get('/login-game',gamePlayController.login);
+
+router.post('/login-game',gamePlayController.loginPost);
+
+router.get('/signup-game',gamePlayController.register);
+
+router.post('/signup-game',gamePlayController.registerPost);
+
+router.get('/logout-game',gamePlayController.logout);
+
+
+
+module.exports = router;
